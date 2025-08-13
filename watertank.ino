74aabf6b2db817d79b683a0ef8a576ac0bf6c7a8
LAKSHMI K <139247602+Lakshmi711@users.noreply.github.com>
2025-08-13 10:04:44 +0530

    watertank.ino

--- watertank.ino
+++ watertank.ino
@@ -8,18 +8,18 @@ int led1=4;
 int led2=5;
 
 void setup() { 
-pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output 
+pinMode(trigPin, OUTPUT); 
 pinMode(echoPin, INPUT); 
 pinMode(led1,OUTPUT);
-pinMode(led2,OUTPUT);// Sets the echoPin as an Input 
-Serial.begin(9600); // Starts the serial communication 
+pinMode(led2,OUTPUT);
+Serial.begin(9600); 
 } 
 
-void loop() { // Clears the trigPin 
+void loop() { 
 triv=digitalRead(trigPin);
 echov=digitalRead(echoPin);
-duration = pulseIn(echov, HIGH); // Calculating the distance 
-distance= duration*0.034/2; // Prints the distance on the 
+duration = pulseIn(echov, HIGH); 
+distance= duration*0.034/2;
 Serial.print("Distance: "); 
 Serial.println(distance); 
 delay(2000);
@@ -40,4 +40,5 @@ else{
   delay(1000);
 }
 }
+
 
